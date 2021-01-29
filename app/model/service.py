0b4b987a3fefe3a13c9cf87eb135e59900e69d4d
J3FALL <crisp.coad@gmail.com>
2021-01-29 14:56:34 +0300

    Fix pep issues

--- app/model/service.py
+++ app/model/service.py
@@ -4,6 +4,7 @@ from .model import Model
 
 
 def all_models() -> List[Model]:
-    models_sample = [Model(model_id='0', label='xgboost', description='xgboost'),
-                     Model(model_id='1', label='KNN', description='KNN')]
+    xgboost = Model(model_id='0', label='xgboost', description='xgboost')
+    knn = Model(model_id='1', label='KNN', description='KNN')
+    models_sample = [xgboost, knn]
     return models_sample
