016a5d9980f6f1d38837cb88f998440ba56c109b
Nikolay Nikitin <nicl.nno@gmail.com>
2021-02-08 01:01:27 +0300

    Code review fixes

--- test/test_meta.py
+++ test/test_meta.py
@@ -1,7 +1,7 @@
 def test_task_names_endpoint(client):
     tasks_json = client.get(f'api/meta/tasks').json
     tasks = [task['task_name'] for task in tasks_json]
-    assert set(['classification',
-                'regression',
-                'ts_forecasting',
-                'clustering']) == set(tasks)
+    assert {'classification',
+            'regression',
+            'ts_forecasting',
+            'clustering'} == set(tasks)
