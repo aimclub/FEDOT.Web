21d6b549197937519495806b334cbdb809457eb5
sanekDa <sanek_015@mail.ru>
2021-02-17 22:30:01 +0800

    added sqlalchemy db creation for tests

--- test/conftest.py
+++ test/conftest.py
@@ -1,9 +1,10 @@
 import pytest
 
-from app import create_app
+from app import create_app, db
 
 
 @pytest.fixture
 def app():
     app = create_app('test')
+    db.create_all(app=app)
     return app
