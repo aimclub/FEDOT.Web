1ba923f34508ff2cda8493233dd4b56bcee66c8e
Nikolay Nikitin <nicl.nno@gmail.com>
2021-07-01 18:55:41 +0300

    Epoch upd

--- app/api/sandbox/service.py
+++ app/api/sandbox/service.py
@@ -3,11 +3,18 @@ from .models import ChainEpochMapping, SandboxDefaultParams
 
 def chains_ids_for_epochs_in_case(case_id):
     chain_ids_by_epochs = {
-        1: 'scoring_chain_1',
-        2: 'scoring_chain_2',
-        3: 'best_scoring_chain'}
-    return [ChainEpochMapping(key, chain_ids_by_epochs[key])
-            for key in chain_ids_by_epochs.keys()]
+        'scoring': {1: 'scoring_chain_1',
+                    2: 'scoring_chain_2',
+                    3: 'best_scoring_chain'},
+        'metocean': {1: 'best_metocean_chain',
+                     2: 'best_metocean_chain',
+                     3: 'best_metocean_chain'},
+        'oil': {1: 'best_oil_chain',
+                2: 'best_oil_chain',
+                3: 'best_oil_chain'}
+    }
+    return [ChainEpochMapping(key, chain_ids_by_epochs[case_id][key])
+            for key in chain_ids_by_epochs[case_id].keys()]
 
 
 def default_params_for_case(case_id):
