c3041e23e4acf0cf5d198ea41466ce00513b55a7
sanekDa <sanek_015@mail.ru>
2021-08-10 00:02:31 +0800

    added deploying script for centos

---
+++ deploy_mongo_centos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+printf '[mongodb-org-5.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/5.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-5.0.asc' > /etc/yum.repos.d/mongodb-org-5.0.repo
+sudo yum install -y mongodb-org
