20cfd01fbd6837745c836e10f5f94d5f296cea05
Александр Абдулов <sanek_015@mail.ru>
2021-08-13 10:41:05 +0800

    added env loading in init_db script

--- init_db.py
+++ init_db.py
@@ -1,12 +1,14 @@
 import os
 
 import pymongo
+from dotenv import load_dotenv
 
 from init.init_cases import create_default_cases
 from init.init_history import create_default_history
 from init.init_pipelines import create_default_pipelines
 
 if __name__ == "__main__":
+    load_dotenv("mongo_conn_string.env")
     env = os.getenv('MONGO_CONN_STRING')
     print(env)
     client = pymongo.MongoClient(env)
