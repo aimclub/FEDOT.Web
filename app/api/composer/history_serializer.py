af661fee031a801cc5cb29ae4a39b38dfdbcd0ad
Sergei Pakulin <pakylin3@mail.ru>
2021-10-08 13:11:12 +0500

    rebuilt abstract serializers interface

--- app/api/composer/history_serializer.py
+++ app/api/composer/history_serializer.py
@@ -1,17 +1,13 @@
 from typing import Any, Dict
 
 from app.interfaces import Serializable
-from fedot.core.optimisers.opt_history import OptHistory
 
 
 class HistorySerializer(Serializable):
 
-    def to_json(self, obj: OptHistory) -> Dict[str, Any]:
-        serialized = vars(obj)
-        serialized["class_path"] = f"{obj.__module__}{Serializable.DELIMITER}{obj.__class__.__qualname__}"
-        return serialized
+    def to_json(self) -> Dict[str, Any]:
+        return super().to_json()
 
-    def from_json(self, json_obj: Dict[str, Any]) -> OptHistory:
-        obj = OptHistory()
-        vars(obj).update(json_obj)
-        return obj
+    @classmethod
+    def from_json(cls, json_obj: Dict[str, Any]):
+        return super().from_json(json_obj)
--- app/interfaces/Serializable.py
+++ app/interfaces/Serializable.py
@@ -1,39 +1,49 @@
 import json
 from abc import ABC, abstractmethod
+from dataclasses import is_dataclass
 from importlib import import_module
 from typing import Any, Dict
 
 DELIMITER = '/'
+CLASS_PATH_KEY = '_class_path'
 
 
 class Serializable(ABC):
 
     @abstractmethod
-    def to_json(self, obj: Any) -> Dict[str, Any]:
-        pass
+    def to_json(self):
+        return {
+            **vars(self),
+            CLASS_PATH_KEY: f'{self.__module__}{DELIMITER}{self.__class__.__qualname__}'
+        }
 
+    @classmethod
     @abstractmethod
-    def from_json(self, json_obj: Dict[str, Any]) -> Any:
-        pass
+    def from_json(cls, json_obj: Dict[str, Any]):
+        obj = cls()
+        del json_obj[CLASS_PATH_KEY]
+        vars(obj).update(json_obj)
+        return obj
 
 
 def encoder(obj: Any) -> Dict[str, Any]:
     if isinstance(obj, Serializable):
         return obj.to_json()
-    raise TypeError(f"{obj=} can't be serialized!")
+    raise TypeError(f'{obj=} can\'t be serialized!')
 
 
-def _get_class_instance(class_path: str) -> Any:
+def _get_class(class_path: str) -> Any:
     module_name, class_name = class_path.split(DELIMITER)
     obj = import_module(module_name)
     for sub in class_name.split('.'):
         obj = getattr(obj, sub)
-    return obj()
+    return obj
 
 
 def decoder(json_obj: Dict[str, Any]) -> Any:
-    if 'class_path' in json_obj:
-        class_instance = _get_class_instance(json_obj['class_path'])
-        if isinstance(class_instance, Serializable):
-            return class_instance.from_json(json_obj)
+    if CLASS_PATH_KEY in json_obj:
+        cls_obj = _get_class(json_obj[CLASS_PATH_KEY])
+        if issubclass(cls_obj, Serializable):
+            return cls_obj.from_json(json_obj)
+        raise TypeError(f'Parsed {cls_obj=} is not serializable, but should be')
     return json_obj
