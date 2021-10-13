66f61e5d10dee2764c5409ec1dfc20138792781c
Sergei Pakulin <pakylin3@mail.ru>
2021-10-13 16:21:04 +0500

    + shared loader schema class

--- app/api/composer/schema.py
+++ app/api/composer/schema.py
@@ -1,3 +1,5 @@
+from abc import ABC
+from inspect import signature
 from typing import Any, List, Mapping
 
 from fedot.core.dag.graph import Graph
@@ -33,80 +35,18 @@ class ComposingHistoryGraphSchema(Schema):
     is_finished = fields.Boolean(attribute='is_finished')
 
 
-class OperationMetaInfoSchema(Schema):
-    """Operation meta info"""
-    __model__ = OperationMetaInfo
+class SchemaLoader(Schema):
 
     @post_load
-    def _deserialize(self, data, **kwargs):
-        return type(self).__model__(**data)
-
-    allowed_positions = fields.List(fields.Str())
-    id = fields.Str()
-    input_types = fields.Function(
-        lambda op: [enum.value for enum in op.input_types],
-        lambda obj: [DataTypesEnum(enum_val) for enum_val in obj['input_types']]
-    )
-    output_types = fields.Function(
-        lambda op: [enum.value for enum in op.output_types],
-        lambda obj: [DataTypesEnum(enum_val) for enum_val in obj['output_types']]
-    )
-    tags = fields.List(fields.Str())
-    task_type = fields.Function(
-        lambda op: [enum.value for enum in op.task_type],
-        lambda obj: [TaskTypesEnum(enum_val) for enum_val in obj['task_type']]
-    )
-
-
-class OperationTypesRepositorySchema(Schema):
-    """Operations type repository"""
-    __model__ = OperationTypesRepository
-
-    @post_load
-    def _deserialize(self, data, **kwargs):
-        return type(self).__model__(**data)
-
-    operations = fields.Nested(OperationMetaInfoSchema, many=True)
-    repository_name = fields.Str()
-    _repo = fields.Nested(OperationMetaInfoSchema, many=True)
-    _tags_excluded_by_default = fields.List(fields.Str())
-
-
-class SkLearnEvaluationStrategySchema(Schema):
-    """SkLearn evaluation strategy"""
-    __model__ = SkLearnEvaluationStrategy
-
-    @post_load
-    def _deserialize(self, data, **kwargs):
-        return type(self).__model__(**data)
-
-    implementation_info = fields.Str()
-    log = fields.Raw()
-    operation_id = fields.Str()
-    operation_type = fields.Str()
-    output_mode = fields.Str()
-    params_for_fit = fields.Raw()  # TODO: what type?
-
-
-class ModelSchema(Schema):
-    """Model"""
-    __model__ = Model
-
-    @post_load
-    def _deserialize(self, data, **kwargs):
-        return type(self).__model__(**data)
-
-    acceptable_task_types = fields.Function(
-        lambda model: [enum.value for enum in model.acceptable_task_types],  # TODO: make fix in FEDOTs enums
-        lambda obj: [TaskTypesEnum(enum_val) for enum_val in obj['acceptable_task_types']]
-    )
-    description = fields.Str()
-    log = fields.Raw()
-    metadata = fields.Nested(OperationMetaInfoSchema)
-    operation_type = fields.Str()
-    operations_repo = fields.Nested(OperationTypesRepositorySchema)
-    params = fields.Str()
-    _eval_strategy = fields.Nested(SkLearnEvaluationStrategySchema)
+    def make_object(self, data, **kwargs):
+        init_data = {
+            k: v
+            for k, v in data.items()
+            if k in signature(type(self).__model__.__init__).parameters
+        }
+        obj = type(self).__model__(**init_data)
+        vars(obj).update(data)
+        return obj
 
 
 class UnionField(fields.Field):
@@ -117,13 +57,10 @@ class UnionField(fields.Field):
         super().__init__()
 
     def _serialize(self, value: Any, attr: str, obj: Any, **kwargs):
-        errors = []
         for field in self.valid_types:
-            try:
-                return field.serialize(value, attr, obj, **kwargs)
-            except ValidationError as error:
-                errors.append(error.messages)
-                raise ValidationError(errors)
+            if field.schema.__model__ == type(value):
+                return field.serialize(attr, obj, **kwargs)
+        raise ValidationError(f"There are no valid schemas to serialize {obj=}")
 
     def _deserialize(
         self, value: Any, attr: str = None, data: Mapping[str, Any] = None, **kwargs
@@ -137,13 +74,88 @@ class UnionField(fields.Field):
                 raise ValidationError(errors)
 
 
-class NodeSchema(Schema):
+class OperationMetaInfoSchema(SchemaLoader):
+    """Operation meta info"""
+    __model__ = OperationMetaInfo
+
+    supported_strategies = UnionField([
+        fields.Dict(fields.Str(), fields.Raw()),
+        fields.Nested('SkLearnEvaluationStrategySchema')
+    ])
+    allowed_positions = fields.List(fields.Str())
+    id = fields.Str()
+    input_types = fields.Function(
+        lambda op: [enum.value for enum in op.input_types],
+        lambda json_arr: [DataTypesEnum(enum_val) for enum_val in json_arr]
+    )
+    output_types = fields.Function(
+        lambda op: [enum.value for enum in op.output_types],
+        lambda json_arr: [DataTypesEnum(enum_val) for enum_val in json_arr]
+    )
+    tags = fields.List(fields.Str())
+    task_type = fields.Function(
+        lambda op: [enum.value for enum in op.task_type],
+        lambda json_arr: [TaskTypesEnum(enum_val) for enum_val in json_arr]
+    )
+
+
+class OperationTypesRepositorySchema(SchemaLoader):
+    """Operations type repository"""
+    __model__ = OperationTypesRepository
+
+    operations = fields.Nested(OperationMetaInfoSchema, many=True)
+    repository_name = fields.Str()
+    _repo = fields.Nested(OperationMetaInfoSchema, many=True)
+    _tags_excluded_by_default = fields.List(fields.Str())
+
+
+class EvaluationStrategySchema(SchemaLoader):
+    """Evaluation strategy"""
+
+    implementation_info = fields.Str()
+
+
+class SkLearnEvaluationStrategySchema(EvaluationStrategySchema):
+    """SkLearn evaluation strategy"""
+    __model__ = SkLearnEvaluationStrategy
+
+    log = fields.Raw()
+    operation_id = fields.Str()
+    operation_type = fields.Str()
+    output_mode = fields.Str()
+    params_for_fit = fields.Raw()  # TODO: what type?
+
+
+class ModelSchema(SchemaLoader):
+    """Model"""
+    __model__ = Model
+
+    acceptable_task_types = fields.Function(
+        lambda model: [enum.value for enum in model.acceptable_task_types],  # TODO: make fix in FEDOTs enums
+        lambda json_arr: [TaskTypesEnum(enum_val) for enum_val in json_arr]
+    )
+    description = fields.Str()
+    log = fields.Raw()
+    metadata = fields.Nested(OperationMetaInfoSchema)
+    operation_type = fields.Str()
+    operations_repo = fields.Nested(OperationTypesRepositorySchema)
+    params = fields.Str()
+    _eval_strategy = fields.Nested(SkLearnEvaluationStrategySchema)
+
+
+class NodeOperatorSchema(SchemaLoader):
+    """Node operator"""
+    __model__ = NodeOperator
+
+    _node = UnionField([
+        fields.Nested('PrimaryNodeSchema', exclude=('_operator',)),
+        fields.Nested('SecondaryNodeSchema', exclude=('_operator',))
+    ])
+
+
+class NodeSchema(SchemaLoader):
     """Node"""
     __model__ = Node
-
-    @post_load
-    def _deserialize(self, data, **kwargs):
-        return type(self).__model__(**data)
 
     content = fields.Nested(ModelSchema)
     # custom_params = fields.Str() TODO: problematic field
@@ -151,7 +163,13 @@ class NodeSchema(Schema):
     distance_to_primary_level = fields.Int()
     fitted_operation = fields.Raw(allow_none=True)  # TODO: what type?
     log = fields.Raw()
-    nodes_from = fields.Nested('SecondaryNodeSchema', many=True)  # TODO: maybe it's great to exclude that from children
+    nodes_from = fields.List(
+        UnionField([
+            fields.Nested('PrimaryNodeSchema', exclude=("nodes_from",)),
+            fields.Nested('SecondaryNodeSchema', exclude=("nodes_from",))
+        ])
+    )  # TODO: maybe it's great to exclude that from children
+    _operator = fields.Nested(NodeOperatorSchema)
     # operation = fields.Str() TODO: problematic field
     rating = fields.Raw(allow_none=True)  # TODO: what type?
 
@@ -164,31 +182,14 @@ class PrimaryNodeSchema(NodeSchema):
     node_data = fields.Dict(fields.Str(), fields.Raw(), required=True)
 
 
-class NodeOperatorSchema(Schema):
-    """Node operator"""
-    __model__ = NodeOperator
-
-    @post_load
-    def _deserialize(self, data, **kwargs):
-        return type(self).__model__(**data)
-
-    _node = fields.Nested('SecondaryNodeSchema', exclude=('_operator',))
-
-
 class SecondaryNodeSchema(NodeSchema):
     """Secondary node"""
     __model__ = SecondaryNode
 
-    _operator = fields.Nested(NodeOperatorSchema)
 
-
-class OperationTemplateSchema(Schema):
+class OperationTemplateSchema(SchemaLoader):
     """Operation template"""
     __model__ = OperationTemplate
-
-    @post_load
-    def _deserialize(self, data, **kwargs):
-        return type(self).__model__(**data)
 
     custom_params = fields.Str()
     fitted_operation = fields.Raw(allow_none=True)  # TODO: what type?
@@ -202,16 +203,12 @@ class OperationTemplateSchema(Schema):
     rating = fields.Raw(allow_nan=True)  # TODO: what type?
 
 
-class GraphSchema(Schema):
+class GraphSchema(SchemaLoader):
     """Graph"""
     __model__ = Graph
 
-    @post_load
-    def _deserialize(self, data, **kwargs):
-        return type(self).__model__(**data)
-
     uid = fields.UUID()
-    nodes = fields.Raw()  # fields.List(UnionField([fields.Nested(PrimaryNodeSchema), fields.Nested(SecondaryNodeSchema)]))
+    nodes = fields.List(UnionField([fields.Nested(PrimaryNodeSchema), fields.Nested(SecondaryNodeSchema)]))
     operator = fields.Nested('GraphOperatorSchema', exclude=('_graph.operator',))
 
 
@@ -236,24 +233,16 @@ class PipelineSchema(GraphSchema):
     template = fields.Nested('PipelineTemplateSchema', allow_none=True)  # TODO: probably not that schema
 
 
-class GraphOperatorSchema(Schema):
+class GraphOperatorSchema(SchemaLoader):
     """Graph operator"""
     __model__ = GraphOperator
-
-    @post_load
-    def _deserialize(self, data, **kwargs):
-        return type(self).__model__(**data)
 
     _graph = fields.Nested(PipelineSchema)
 
 
-class PipelineTemplateSchema(Schema):
+class PipelineTemplateSchema(SchemaLoader):
     """Pipeline template"""
     __model__ = PipelineTemplate
-
-    @post_load
-    def _deserialize(self, data, **kwargs):
-        return type(self).__model__(**data)
 
     computation_time = fields.Float(allow_nan=True)
     depth = fields.Int()
@@ -265,13 +254,9 @@ class PipelineTemplateSchema(Schema):
     unique_pipeline_id = fields.UUID()  # TODO: str(uuid4()) if not pipeline.uid else pipeline.uid
 
 
-class ParentOperatorSchema(Schema):
+class ParentOperatorSchema(SchemaLoader):
     """Parent operator"""
     __mode__ = ParentOperator
-
-    @post_load
-    def _deserialize(self, data, **kwargs):
-        return type(self).__model__(**data)
 
     operator_name = fields.Str()
     operator_type = fields.Str()
@@ -279,30 +264,22 @@ class ParentOperatorSchema(Schema):
     uid = fields.UUID()
 
 
-class IndividualSchema(Schema):
+class IndividualSchema(SchemaLoader):
     """Individual"""
     __model__ = Individual
-
-    @post_load
-    def _deserialize(self, data, **kwargs):
-        return type(self).__model__(**data)
 
     parent_operators = fields.Nested(ParentOperatorSchema, many=True)
     fitness = fields.Float()
     graph = fields.Nested(PipelineTemplateSchema)
 
 
-class OptHistorySchema(Schema):
+class OptHistorySchema(SchemaLoader):
     """OptHistory"""
     __model__ = OptHistory
 
-    @post_load
-    def _deserialize(self, data, **kwargs):
-        return type(self).__model__(**data)
-
     metrics = fields.Raw()  # TODO: store function call?!
-    # individuals = fields.List(fields.Nested(IndividualSchema, many=True))
-    # archive_history = fields.List(fields.Nested(IndividualSchema, many=True))
+    individuals = fields.List(fields.Nested(IndividualSchema, many=True))
+    archive_history = fields.List(fields.Nested(IndividualSchema, many=True))
     pipelines_comp_time_history = fields.List(fields.Raw())
     archive_comp_time_history = fields.List(fields.Raw())
     parent_operators = fields.List(fields.List(fields.Nested(ParentOperatorSchema, many=True)))
