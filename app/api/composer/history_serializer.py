ffdd9edac878c513b4772697ebcdc94dfedf5829
Sergei Pakulin <pakylin3@mail.ru>
2021-10-15 17:41:57 +0500

    serializers update

--- app/api/composer/history_serializer.py
+++ app/api/composer/history_serializer.py
@@ -1,5 +1,6 @@
 from importlib import import_module
-from inspect import isclass
+from inspect import isclass, signature
+from pathlib import Path
 from typing import Any, Dict
 
 from app.interfaces.serializable import (CLASS_PATH_KEY, DELIMITER,
@@ -22,8 +23,7 @@ class OperationMetaInfoSerializer(Serializable):
         basic_serialization = super().to_json()
         strategy = basic_serialization['supported_strategies']
         if isclass(strategy):
-            basic_serialization['supported_strategies'] = f'\
-                {strategy.__module__}{DELIMITER}{strategy.__qualname__}'
+            basic_serialization['supported_strategies'] = f'{strategy.__module__}{DELIMITER}{strategy.__qualname__}'
         return basic_serialization
 
     @classmethod
@@ -37,11 +37,26 @@ class LogSerializer(Serializable):
     def to_json(self) -> Dict[str, Any]:
         basic_serialization = super().to_json()
         del basic_serialization['logger']  # cause it will be automatically generated in __init__
+        basic_serialization['logger_name'] = basic_serialization['name']
+        basic_serialization['config_json_file'] = basic_serialization['config_file']
+        del basic_serialization['config_file']
+        basic_serialization['output_verbosity_level'] = basic_serialization['verbosity_level']
+        del basic_serialization['verbosity_level']
         return basic_serialization
 
     @classmethod
     def from_json(cls, json_obj: Dict[str, Any]):
-        return super().from_json(json_obj)
+        init_data = {
+            k: v
+            for k, v in json_obj.items()
+            if k in signature(cls.__init__).parameters
+        }
+        config_json_file, log_file = init_data['config_json_file'], init_data['log_file']
+        if config_json_file != 'default' and not Path(config_json_file).exists():
+            init_data['config_json_file'] = 'default'
+        if init_data['config_json_file'] == 'default' and not Path(log_file).exists():
+            init_data['log_file'] = None
+        return super().from_json(init_data)
 
 
 class EnumSerializer(Serializable):
@@ -54,4 +69,6 @@ class EnumSerializer(Serializable):
 
     @classmethod
     def from_json(cls, json_obj: Dict[str, Any]):
+        if json_obj['value'] in [['classification'], ['regression']]:
+            return cls(tuple(json_obj["value"]))
         return cls(json_obj["value"])
--- app/interfaces/Serializable.py
+++ app/interfaces/Serializable.py
@@ -27,8 +27,11 @@ class Serializable(ABC):
             for k, v in json_obj.items()
             if k in signature(cls.__init__).parameters
         }
-        obj = cls(init_data)
-        vars(obj).update(json_obj)
+        obj = cls(**init_data)
+        vars(obj).update({
+            k: json_obj[k]
+            for k in json_obj.keys() ^ init_data.keys()
+        })
         return obj
 
 
