662380a60942ba34af9604f84c83b6332ec730a2
morrisnein <petroochcho@gmail.com>
2022-08-02 12:55:15 +0300

    fix deploy scripts

--- scripts/deploy_fedot_web.sh
+++ scripts/deploy_fedot_web.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 cd ..
 # backend
-export MONGO_CONN_STRING="mongodb://127.0.0.1:27017/test_db?compressors=disabled&gssapiServiceName=mongodb"
-sh ./scripts/start_mongo.sh
+export MONGO_CONN_STRING="mongodb://127.0.0.1:27017/test_db?compressors=disabled"
+# sh ./scripts/start_mongo.sh
+
 ./venv/bin/python ./init_db.py
 # frontend
-export REACT_APP_BASE_URL="https://fedot.onti.actcognitive.org:443"
-(cd ./frontend || return; npm install && yarn build)
+export REACT_APP_BASE_URL=https://fedot.onti.actcognitive.org:443
+(cd ./frontend || return; npm install && npm audit fix && npx browserslist@latest --update-db && yarn build)
 git restore ./frontend/build/static/cases_icons/*
 # start
+export FLASK_HOST=10.32.1.9
+export FLASK_PORT=5000
 ./venv/bin/python ./main.py
