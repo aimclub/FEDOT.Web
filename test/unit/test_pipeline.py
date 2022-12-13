b66d6488cb037463dd70be3d2db513aafb0da81f
morrisnein <petroochcho@gmail.com>
2022-12-13 14:34:21 +0300

    fix typo

--- test/unit/test_pipeline.py
+++ test/unit/test_pipeline.py
@@ -3,7 +3,7 @@ from app.api.pipelines.service import pipeline_by_uid
 from app.api.showcase.showcase_utils import showcase_item_from_db
 
 
-def test_load_fitted_pipeline_pipeline(app):
+def test_load_fitted_pipeline(app):
     pipeline, case = pipeline_by_uid('best_scoring_pipeline'), showcase_item_from_db('scoring')
     train_data = get_input_data(dataset_name='scoring', sample_type='train', task_type=case.metadata.task_name)
     prediction = pipeline.predict(train_data)
