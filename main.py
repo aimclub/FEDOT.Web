cf4a3af4cef99e3d0c830dd617c97630ddea5361
Michael Linchevsky <linchevskiy.misha@mail.ru>
2023-09-19 23:51:13 +0300

    refactored styles pointed by PEP 8 issues #4

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
 
     app = create_app(os.getenv("FLASK_ENV") or "dev")
     app.add_url_rule('/download_pipeline', 'download_pipeline',
-        download_pipeline, methods=['POST'])  # Route registration
+                        download_pipeline, methods=['POST'])  # Route registration
 
     app.wsgi_app = ProxyFix(app.wsgi_app)
 
